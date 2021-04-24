ab8819ac70ac23105acea7182b99eae9d267527d
Bonn <ryan.agharaad@gmail.com>
2021-04-24 09:21:27 -0400

    Hello World

---
+++ src/App.java
@@ -0,0 +1,10 @@
+// main is always used in java to execute a command
+// every Java program has a class name which must match the filename, and that every program must contain the main() method.
+// println is short for print line which prints on a new line
+// print shows on the same line instead
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}

65cecf9787504e409157076fab1c0683cce52457
Bonn <ryan.agharaad@gmail.com>
2021-04-25 22:32:07 -0400

    Starting off slow with these place holders for my currency converter

---
+++ src/currency_converter.java
@@ -0,0 +1,14 @@
+
+
+import java.util.*;
+import java.text.DecimalFormat;
+
+public class currency_exchange {
+    public static void main(String[] args) {
+        double amount, dollar, pound, euro, yen, rupee;
+
+        DecimalFormat f = new DecimalFormat("##.##");
+    }
+
+}
+}
